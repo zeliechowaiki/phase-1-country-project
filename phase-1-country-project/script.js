58a9f474d931ec947d4760a3087121d77d934268
Tunisia Artope <artope@yahoo.com>
2022-02-15 15:56:17 -0500

    second commit

--- phase-1-country-project/script.js
+++ phase-1-country-project/script.js
@@ -1,3 +1,16 @@
-// const h2 = document.createElement("h2");
-// h2.textContent = "This content added by JavaScript";
-// document.querySelector("body").appendChild(h2);
+fetch('https://restcountries.com/v3.1/all')
+    .then(response => response.json())
+    .then(countryData => {
+        console.log (countryData)
+        countryData.forEach(country => {
+            createCountryNav(country)
+        })
+    })
+
+let flags = document.getElementById('flags')
+// console.log(flags)
+function createCountryNav(country){
+    let pic = document.createElement('img')
+    pic.src = country.flags.png
+    flags.appendChild(pic)
+}
